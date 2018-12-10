5d60cdf5318b8e742654cb4acf5919d48f35789a
Tony <30836715+tonyp14@users.noreply.github.com>
2018-12-10 09:52:52 +0530

    Delete read_mysql_table.py

--- read_mysql_table.py
+++
@@ -1,9 +0,0 @@
-#read data from mysql table in python
-import pandas as pd
-import pymysql
-from sqlalchemy import create_engine
-
-engine = create_engine('mysql+pymysql://root:Assyst@123@localhost/assignments')
-
-df = pd.read_sql_query('SELECT * FROM employee', engine)
-print(df)
